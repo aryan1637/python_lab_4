e145061d4270aacf8b7ebb33bcb2e44a8862b496
aryan1637 <bca2022aryankumar1637@tnraocollege.org>
2024-08-11 19:55:00 +0530

    Add files via upload

---
+++ dictionary_mtd.py
@@ -0,0 +1,16 @@
+emp = {
+    'id': 1,
+    'name': 'Aryan Patel',
+    'designation': 'Manager',
+    'salary': 60000
+}
+
+print("Dictionary:",emp)
+
+# Delete the 'designation' key
+del emp['designation']
+print("\nDeleting Designation:",emp)
+
+# Update the 'name' field
+emp['name'] = 'John Doe'
+print("\nUpdating 'name':",emp)
